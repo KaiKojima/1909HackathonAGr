ca247d317dcac85eb8fa64cea887bc5c579e1e9b
ShotaKudo <55224900+ShotaKudo@users.noreply.github.com>
2019-09-27 14:07:10 +0900

    コンソールだけです

---
+++ bj/Bj_main.java
@@ -0,0 +1,126 @@
+package bj;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Bj_main {
+
+	public static void main(String[] args) {
+
+		Card c = new Card();
+		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+
+			System.out.println("ゲームを開始します");
+			//空の山札を作成
+			List<Integer> deck = new ArrayList<>(52);
+			//山札をシャッフル
+			c.shuffleDeck(deck);
+
+			//プレイヤー・ディーラーの手札リストを生成
+			List<Integer> player = new ArrayList<>();
+			List<Integer> dealer = new ArrayList<>();
+
+			//プレイヤー・ディーラーがカードを2枚引く
+			player.add(deck.get(0));
+			dealer.add(deck.get(1));
+			player.add(deck.get(2));
+			dealer.add(deck.get(3));
+
+			//山札の進行状況を記録する変数deckCountを定義
+			int deckCount = 4;
+
+			//プレイヤーの手札枚数を記録する変数playerHandsを定義
+			int playerHands = 2;
+
+			//プレイヤー・ディーラーの手札のポイントを表示
+			System.out.println();
+			System.out.println("あなたの1枚目のカードは" + c.toDescription(player.get(0)));
+			System.out.println();
+
+			System.out.println("ディーラーの1枚目のカードは" + c.toDescription(dealer.get(0)));
+			System.out.println();
+
+			System.out.println("あなたの2枚めのカードは" + c.toDescription(player.get(1)));
+			System.out.println();
+
+			System.out.println("ディーラーの2枚めのカードは秘密です。");
+			System.out.println();
+
+			//プレイヤー・ディーラーのポイントを集計
+			int playerPoint = c.sumPoint(player);
+			int dealerPoint = c.sumPoint(dealer);
+
+			System.out.println("あなたの現在のポイントは" + playerPoint + "です。");
+
+			//プレイヤーがカードを引くフェーズ
+			while (true) {
+				System.out.println("カードを引きますか？Yes:y or No:n");
+				Scanner scan = new Scanner(System.in);
+				String str = scan.next();
+
+				if ("n".equals(str)) {
+					break;
+				} else if ("y".equals(str)) {
+					//手札に山札から1枚加える
+					player.add(deck.get(deckCount));
+					//山札と手札を一枚進める
+					deckCount++;
+					playerHands++;
+
+					System.out.println("あなたの" + playerHands + "枚目のカードは" + c.toDescription(player.get(playerHands - 1)));
+					playerPoint = c.sumPoint(player);
+					System.out.println("現在の合計は" + playerPoint);
+					//プレイヤーのバーストチェック
+					if (c.isBusted(playerPoint)) {
+						System.out.println("残念、バーストしてしまいました。");
+						return;
+					}
+				} else {
+					System.out.println("あなたの入力は" + str + " です。y か n を入力してください。");
+				}
+			}
+
+			//ディーラーが手札を17以上にするまでカードを引くフェーズ
+			while (true) {
+				//手札が17以上の場合ブレーク
+				if (dealerPoint >= 17) {
+					break;
+				} else {
+					//手札に山札から1枚加える
+					dealer.add(deck.get(deckCount));
+					//山札を一枚進める
+					deckCount++;
+
+					//ディーラーの合計ポイントを計算
+					dealerPoint = c.sumPoint(dealer);
+					//ディーラーのバーストチェック
+					if (c.isBusted(dealerPoint)) {
+						System.out.println("ディーラーがバーストしました。あなたの勝ちです！");
+						return;
+					}
+
+				}
+
+			}
+			System.out.println("あなたのポイントは" + playerPoint);
+			System.out.println("ディーラーのポイントは" + dealerPoint);
+			System.out.println();
+
+			if (playerPoint == dealerPoint) {
+				System.out.println("引き分けです。");
+
+			} else if (playerPoint > dealerPoint) {
+				System.out.println("勝ちました！");
+
+			} else {
+				System.out.println("負けました・・・");
+
+			}
+
+		}
+
+	}
+}
